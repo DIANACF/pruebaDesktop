d513d6c0806e8cf1e4986156a8b489d7e10d0a93
RinconAura <acrincon@uniboyaca.edu.co>
2024-09-19 16:58:22 -0500

    Primer Cambio

---
+++ pruebaD/pruebaD/Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // Incluir la biblioteca iostream para entrada/salida
+
+int main() {
+    // Código del programa
+    std::cout << "Hola, mundo!" << std::endl; // Imprimir un mensaje en la consola
+
+    return 0; // Indicar que el programa terminó correctamente
+}
